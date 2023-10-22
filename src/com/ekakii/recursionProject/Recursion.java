e139f1510181ed91c8f7f2866b10d8647de27dc0
Abhay Manoj <sanicthecheese@gmail.com>
2023-10-21 22:25:12 -0400

    Added pattern recursion

--- src/com/ekakii/recursionProject/Recursion.java
+++ src/com/ekakii/recursionProject/Recursion.java
@@ -35,21 +35,21 @@ public class Recursion {
      * @Date October 20, 2023
      * @Modified October 20, 2023
      * @Description Checks if it is possible for the character counts to be a palindrome
-     * @Parameters valueIterator - goes through the values from its hashmap, hasOddCount - if a character with an odd number of occurrences has already been found
+     * @Parameters valueIterator - goes through the values from its hashmap, hasOddCountBeenFound - if a character with an odd number of occurrences has already been found
      * @Returns If the character counts can be a palindrome, Data Type: Boolean
      * Dependencies: HashMap, Iterator
      * Throws/Exceptions: N/A
      */
 
-    private static boolean checkCharacterCounts(Iterator<Integer> valueIterator, boolean hasOddCount) {
+    private static boolean checkCharacterCounts(Iterator<Integer> valueIterator, boolean hasOddCountBeenFound) {
         if (!valueIterator.hasNext()) return true;
         if (valueIterator.next() % 2 == 1) {
-            if (hasOddCount) return false;
+            if (hasOddCountBeenFound) return false;
             else return checkCharacterCounts(valueIterator, true);
-        } return checkCharacterCounts(valueIterator, hasOddCount);
+        } return checkCharacterCounts(valueIterator, hasOddCountBeenFound);
     }
 
-    /** Method Name: isPalindrome
+    /** Method Name: possiblePal
      * @Author Abhay Manoj
      * @Date October 20, 2023
      * @Modified October 20, 2023
@@ -60,11 +60,48 @@ public class Recursion {
      * Throws/Exceptions: N/A
      */
 
-    public static boolean isPalindrome(String givenString) {
+    public static boolean possiblePal(String givenString) {
         HashMap<Character, Integer> countOfCharacters = new HashMap<>(); // contains each character of the string along with its number of occurrences
         fillHashMapWithCount(countOfCharacters, givenString, givenString.length() - 1);
         Iterator<Integer> valueIterator = countOfCharacters.values().iterator(); // used to access values of hashmap
         return checkCharacterCounts(valueIterator, false);
+    }
+
+    /** Method Name: printChar
+     * @Author Abhay Manoj
+     * @Date October 21, 2023
+     * @Modified October 21, 2023
+     * @Description Prints out a character the specified number of times
+     * @Parameters character - character to be printed, i - how many times to print it out
+     * @Returns N/A, Data Type: Void
+     * Dependencies: N/A
+     * Throws/Exceptions: N/A
+     */
+
+    private static void printChar(char character, int i) {
+        if (i == 0) return;
+        System.out.print(character);
+        printChar(character, i - 1);
+    }
+
+    /** Method Name: pattern
+     * @Author Abhay Manoj
+     * @Date October 21, 2023
+     * @Modified October 21, 2023
+     * @Description Prints out a fractal pattern
+     * @Parameters numberOfStars - number of stars to be printed, buffer - the amount of empty characters before the stars
+     * @Returns N/A, Data Type: Void
+     * Dependencies: N/A
+     * Throws/Exceptions: N/A
+     */
+
+    public static void pattern(int numberOfStars, int buffer) {
+        if (numberOfStars == 0) return;
+        pattern(numberOfStars / 2, buffer);
+        printChar(' ', buffer);
+        printChar('*', numberOfStars);
+        System.out.println();
+        pattern(numberOfStars / 2, buffer + numberOfStars / 2);
     }
 
     /** Method Name: main
@@ -79,6 +116,6 @@ public class Recursion {
      */
 
     public static void main(String[] args) {
-        System.out.println(isPalindrome("racecar"));
+        pattern(16,2);
     }
 }
