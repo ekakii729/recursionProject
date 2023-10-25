5da6e1b74ae6e7d6bdb90411a831c9c42e4ad456
Abhay Manoj <sanicthecheese@gmail.com>
2023-10-25 19:56:56 -0400

    Finished!

--- src/com/ekakii/recursionProject/Recursion.java
+++ src/com/ekakii/recursionProject/Recursion.java
@@ -1,11 +1,12 @@
 /*
  * Author: Abhay Manoj
- * Purpose:
+ * Purpose: A program to see if a string can be a palindrome, print a fractal pattern, and find amount of blobs in a file. Contains NO loops at all.
  * Date of Creation: October 20, 2023
  */
 
 package com.ekakii.recursionProject;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -65,6 +66,7 @@ public class Recursion {
      */
 
     private static boolean possiblePal(String givenString) {
+        if (givenString.length() == 2) return true;
         HashMap<Character, Integer> countOfCharacters = new HashMap<>(); // contains each character of the string along with its number of occurrences
         fillHashMapWithCount(countOfCharacters, givenString, givenString.length() - 1);
         Iterator<Integer> valueIterator = countOfCharacters.values().iterator(); // used to access values of hashmap
@@ -174,7 +176,7 @@ public class Recursion {
      */
 
     private static int getBlobSize(int currentRow, int currentColumn, char[][] characterArray, boolean[][] charHasBeenVisited) {
-        if (currentRow < 0 || currentRow >= characterArray.length || currentColumn < 0 || currentColumn >= characterArray[0].length || charHasBeenVisited[currentRow][currentColumn] || characterArray[currentRow][currentColumn] == '.') return 0;
+        if (currentRow < 0 || currentRow >= characterArray.length || currentColumn < 0 || currentColumn >= characterArray[0].length || charHasBeenVisited[currentRow][currentColumn] || characterArray[currentRow][currentColumn] != 'X') return 0;
         charHasBeenVisited[currentRow][currentColumn] = true;
         int blobSize = 1; // amount of characters within the 'blob'
         blobSize += getBlobSize(currentRow + 1, currentColumn, characterArray, charHasBeenVisited);
@@ -199,7 +201,7 @@ public class Recursion {
         if (currentColumn == characterArray[0].length) return 0;
         if (charHasBeenVisited[currentRow][currentColumn] || characterArray[currentRow][currentColumn] != 'X') return checkShapeColumns(currentRow, currentColumn + 1, characterArray, charHasBeenVisited);
         getBlobSize(currentRow, currentColumn, characterArray, charHasBeenVisited);
-        return 1 + checkShapeColumns(currentRow, currentColumn + 1, characterArray, charHasBeenVisited);
+        return checkShapeColumns(currentRow, currentColumn + 1, characterArray, charHasBeenVisited) + 1;
     }
 
     /** Method Name: checkShapeRows
@@ -215,8 +217,7 @@ public class Recursion {
 
     private static int checkShapeRows(char[][] characterArray, boolean[][] charHasBeenVisited, int currentRow) {
         if (currentRow == characterArray.length) return 0;
-        int size = checkShapeColumns(currentRow, 0, characterArray, charHasBeenVisited);
-        return size + checkShapeRows(characterArray, charHasBeenVisited, currentRow + 1);
+        return checkShapeColumns(currentRow, 0, characterArray, charHasBeenVisited) + checkShapeRows(characterArray, charHasBeenVisited, currentRow + 1);
     }
 
     /** Method Name: getNumberOfBlobs
@@ -226,13 +227,13 @@ public class Recursion {
      * @Description Gets the number of blobs within one shape
      * @Parameters reader - used to read the file
      * @Returns The number of blobs within one shape, Data Type: Integer
-     * Dependencies: N/A
+     * Dependencies: BufferedReader
      * Throws/Exceptions: IOException
      */
 
     private static int getNumberOfBlobs(BufferedReader reader) throws IOException {
-        char[][] characterArray = createCharArray(reader);
-        boolean[][] charHasBeenVisited = new boolean[characterArray.length][characterArray[0].length];
+        char[][] characterArray = createCharArray(reader); // creates an array of the shape
+        boolean[][] charHasBeenVisited = new boolean[characterArray.length][characterArray[0].length]; // for checking if individual characters have been visited
         return checkShapeRows(characterArray, charHasBeenVisited, 0);
     }
 
@@ -248,73 +249,69 @@ public class Recursion {
      */
 
     private static void printShapeData(BufferedReader reader, int currentShapeIndex, int numberOfShapes) throws IOException {
-        if (currentShapeIndex == numberOfShapes) return;
-        System.out.printf("In shape #%d, there is %d blobs.\n", currentShapeIndex + 1, getNumberOfBlobs(reader));
+        if (currentShapeIndex == numberOfShapes) {
+            System.out.println();
+            return;
+        } System.out.printf("In shape #%d, there is %d blobs.\n", currentShapeIndex + 1, getNumberOfBlobs(reader));
         printShapeData(reader, currentShapeIndex + 1, numberOfShapes);
     }
 
-    /** Method Name: printShapeData
+    /** Method Name: countShapes
      * @Author Abhay Manoj
      * @Date October 25, 2023
      * @Modified October 25, 2023
-     * @Description Prints out the number of blobs within one shape
-     * @Parameters reader - used to read the file, currentShapeIndex - the shape that is being looked at, numberOfShapes - how many shapes are in the file
+     * @Description Prints out the number of blobs in all shapes in a file
+     * @Parameters input - used to take user input, fileName - name of the file
      * @Returns N/A, Data Type: N/A
-     * Dependencies: BufferedReader
-     * Throws/Exceptions: IOException
+     * Dependencies: BufferedReader, Scanner
+     * Throws/Exceptions: RuntimeException
      */
 
     private static void countShapes(Scanner input, String fileName) {
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            BufferedReader reader = new BufferedReader(new FileReader(fileName)); // to read the file
             System.out.print("Enter number of shapes within the file: ");
-            int numberOfShapes = Integer.parseInt(input.nextLine());
-            System.out.println();
-            printShapeData(reader, 0, numberOfShapes);
-            System.out.println();
+            printShapeData(reader, 0, Integer.parseInt(input.nextLine()));
             reader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found! Try retyping it.\n");
         } catch (IOException e) {
             System.out.println("IO ERROR --> " + e);
             throw new RuntimeException(e);
+        } catch (NumberFormatException e) {
+            System.out.println("You have entered an incorrect number of shapes... recount next time.\n");
         }
     }
 
-    /** Method Name: printShapeData
+    /** Method Name: menuLoop
      * @Author Abhay Manoj
      * @Date October 25, 2023
      * @Modified October 25, 2023
-     * @Description Prints out the number of blobs within one shape
-     * @Parameters reader - used to read the file, currentShapeIndex - the shape that is being looked at, numberOfShapes - how many shapes are in the file
+     * @Description The menu loop of the program
+     * @Parameters input - used to take user input, canRun - determines if the program can run
      * @Returns N/A, Data Type: N/A
-     * Dependencies: BufferedReader
-     * Throws/Exceptions: IOException
+     * Dependencies: Scanner
+     * Throws/Exceptions: N/A
      */
 
     private static void menuLoop(Scanner input, boolean canRun) {
         if (!canRun) System.exit(0);
         System.out.println("Please select an option.\n1. Check if a String can be a palindrome\n2. Print a cool fractal pattern\n3. Get the number of blobs from a file\n4. Quit\n");
-        int choice = Integer.parseInt(input.nextLine());
-        switch (choice) {
+        switch (Integer.parseInt(input.nextLine())) {
             case 1 -> {
-                System.out.print("Enter the string you wish to check: ");
-                boolean canBePalindrome = possiblePal(input.nextLine());
-                String response = canBePalindrome ? "Yes, that can be a palindrome.\n" : "No, that cannot be a palindrome.\n";
+                System.out.print("Enter the word you wish to check: ");
+                String response = possiblePal(input.nextLine()) ? "Yes, that can be a palindrome.\n" : "No, that cannot be a palindrome.\n"; // if the string can be palindrome
                 System.out.println(response);
-            }
-            case 2 -> {
-                System.out.print("For the length of the fractal, enter a number that is a power of two: ");
-                int numberOfStars = Integer.parseInt(input.nextLine());
+            } case 2 -> {
+                System.out.print("For the length of the fractal, enter a number that is a power of two: "); // works even if it isn't power of two
+                int numberOfStars = Integer.parseInt(input.nextLine()); // number of stars in the longest line in the fractal
                 System.out.print("Enter the size of buffer between the console and the left hand side of the fractal: ");
-                int bufferSize = Integer.parseInt(input.nextLine());
-                System.out.println();
+                int bufferSize = Integer.parseInt(input.nextLine()); // description is right above
                 pattern(numberOfStars, bufferSize);
-                System.out.println();
-            }
-            case 3 -> {
+            } case 3 -> {
                 System.out.print("Enter EXACT name of the file (ex. myFile.txt): ");
                 countShapes(input, input.nextLine());
-            }
-            case 4 -> {
+            } case 4 -> {
                 System.out.println("Program has been closed.");
                 menuLoop(input, false);
             }
@@ -327,9 +324,8 @@ public class Recursion {
      * @Modified October 20, 2023
      * @Description main method of the program
      * @Parameters args - arguments to be passed in
-     * @Returns N/A, Data
-     * Type: Void
-     * Dependencies: N/A
+     * @Returns N/A, Data Type: Void
+     * Dependencies: Scanner
      * Throws/Exceptions: N/A
      */
 
